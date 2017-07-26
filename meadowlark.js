dfda5aec9ea4bb1c220ce47601f48299b3bab929
shen <shen@macdeMacBook-Pro-4.local>
2017-07-26 19:31:55 +0800

    5.7　跨页测试

--- meadowlark.js
+++ meadowlark.js
@@ -31,6 +31,12 @@ app.get('/about', function(req, res) {
         pageTestScript: '/qa/tests-about.js' 
     });
 });
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river'); 
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+}); 
 
 // 404 catch-all处理器（中间件）
 app.use(function(req, res, next){ 
