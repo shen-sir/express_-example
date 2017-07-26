353115d21bef1d5fff12b5b0a80f247590e88496
shen <shen@macdeMacBook-Pro-4.local>
2017-07-26 18:24:30 +0800

    5.6　页面测试

--- meadowlark.js
+++ meadowlark.js
@@ -15,11 +15,21 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+                    req.query.test === '1';
+    next();
+});
+
 app.get('/', function(req, res){ 
            res.render('home');
 });
-app.get('/about', function(req, res){
-  res.render('about', { fortune: fortune.getFortune() });
+app.get('/about', function(req, res) { 
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js' 
+    });
 });
 
 // 404 catch-all处理器（中间件）
