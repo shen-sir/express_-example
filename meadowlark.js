416ef7ee1b0063e7f9c0a4f38ea0926db6455ea0
shen <shen@macdeMacBook-Pro-4.local>
2017-07-26 16:54:44 +0800

    3.3.3　视图中的动态内容

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,13 @@ var express = require('express');
 
 var app = express();
 
+var fortunes = [
+  "Conquer your fears or they will conquer you.", 
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+]; 
+
 // 设置handlebars视图引擎
 var handlebars = require('express3-handlebars')
             .create({ defaultLayout:'main' });
@@ -18,8 +25,9 @@ app.get('/', function(req, res){
            res.render('home');
 });
 app.get('/about', function(req, res){
-            res.type('text/plain');
-            res.send('About Meadowlark Travel');
+            var randomFortune =
+          fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
 });
 
 // 404 catch-all处理器（中间件）
